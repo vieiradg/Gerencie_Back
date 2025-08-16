6c489deb8d3d427ec1cc260c1f171f0991525f61
PatrickLAF2k <patrickleonardoaf@gmail.com>
2025-08-16 15:23:31 -0300

    Adiciona o modelo de usuário com campos obrigatórios e complementares

---
+++ src/model/user_model.py
@@ -0,0 +1,24 @@
+from src.model import db
+from sqlalchemy.schema import Column
+from sqlalchemy.types import String, Integer
+
+
+class userModel(db.Model):
+    __tablename__ = 'user'
+
+    # campos obrigatorios de cadfastro
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(100), nullable=False)
+    email = Column(String(100), nullable=False, unique=True)
+    password = Column(String(100), nullable=False)
+    cpf = Column(String(11), nullable=False, unique=True)
+    
+    # campos para cadastro complementar
+    occupation = Column(String(50), nullable=True)
+    marital_status = Column(String(20), nullable=True)
+    postal_code = Column(String(10), nullable=True)
+    house_number = Column(String(10), nullable=True)
+    rg = Column(String(20), nullable=True, unique=True)
+    
+    def to_dict(self) -> dict:
+        return {"id": self.id}
