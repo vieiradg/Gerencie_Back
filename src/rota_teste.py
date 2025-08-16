c1bbb62244b4745a1e53fd63117a7319998009be
PatrickLAF2k <patrickleonardoaf@gmail.com>
2025-08-16 15:23:08 -0300

    Adiciona a rota de teste para verificar a conexão com o banco de dados PostgreSQL

---
+++ src/rota_teste.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, request, jsonify  # Importa módulos do Flask
+from src.model.user_model import userModel
+from src.model import db
+from sqlalchemy import text
+bp_teste = Blueprint('teste', __name__)
+
+@bp_teste.route('/teste', methods=['GET'])
+def teste():
+    db_status = "Não testado ainda"
+    
+    try:
+        # Consulta mínima apenas para testar conexão
+        result = db.session.execute(text("SELECT 1")).scalar()
+        if result == 1:
+            db_status = "Conexao com Postgres OK"
+        else:
+            db_status = "Conexao estabelecida, mas retorno inesperado"
+    except Exception as e:
+        db_status = f"Erro ao conectar no banco: {str(e)}"
+    
+    return jsonify({
+        "mensagem": "Parabans, o servidor esta funcionando!",
+        "db_status": db_status
+    })
