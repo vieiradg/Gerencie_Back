f6ab679d6a335561092700a973df0c3f48e2265d
PatrickLAF2k <patrickleonardoaf@gmail.com>
2025-08-18 19:22:01 -0300

    Criado a logica para o hash da senha

--- src/controller/user/user_register.py
+++ src/controller/user/user_register.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, request, jsonify  # Importa módulos do Flask
+from sqlalchemy import or_
+
 from src.model.user_model import userModel
 from src.model import db
-from sqlalchemy import or_
+from src.security.bcrypt_config import hash_password, check_password
 
 # Cria um blueprint para rotas relacionadas ao usuário
 bp_user = Blueprint("user", __name__, url_prefix="/user")
@@ -27,12 +29,16 @@ def register():
      user = userModel(
           name=data["name"],
           email=data["email"],
-          password=data["password"],  # mais tarde pode usar hash de senha
+          password=hash_password(data["password"]),  # mais tarde pode usar hash de senha
           cpf=data["cpf"]
      )
 
      # adiciona no banco
-     db.session.add(user)
-     db.session.commit()
-
-     return jsonify({"message": "Usuário cadastrado com sucesso", "id": user.id}), 201
+     try:
+          db.session.add(user)
+          db.session.commit()
+          return jsonify({"message": "Usuário cadastrado com sucesso", "id": user.id}), 201
+     except Exception as e:
+          db.session.rollback()
+          return jsonify({"message": "Erro ao cadastrar usuário", "erro": str(e)}), 500
+          
