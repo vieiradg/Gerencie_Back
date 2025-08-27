c8f51bef518bd7f7b17f536e40f8e537573d37da
PatrickLAF2k <patrickleonardoaf@gmail.com>
2025-08-27 20:24:36 -0300

    Atualiza a importação dos módulos de usuário para user_login e user_register

--- src/controller/user/__init__.py
+++ src/controller/user/__init__.py
@@ -3,4 +3,4 @@ from flask import Blueprint
 bp_user = Blueprint("user", __name__, url_prefix="/user")
 
 # Importa os módulos que registram rotas
-from . import register, login
+from . import user_login, user_register
