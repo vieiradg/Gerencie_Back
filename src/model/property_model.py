f90156d32850e1451f6f37a77432ee3bf4057800
PatrickLAF2k <patrickleonardoaf@gmail.com>
2025-09-17 20:34:37 -0300

    Adicionado a propriedade nomde do imovel e  atualizado o modelo das propriedades

--- src/model/property_model.py
+++ src/model/property_model.py
@@ -7,18 +7,23 @@ class propertyModel(db.Model):
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
-    house_name = Column(String(25), nullable=True )
-    postal_code = Column(String(10), nullable=False)
+    house_street = Column(String(100), nullable=False)
     house_number = Column(String(10), nullable=False)
     house_complement = Column(String(50), nullable=True)
-
-
+    city = Column(String(50), nullable=False)
+    house_neighborhood = Column(String(50), nullable=False)
+    postal_code = Column(String(10), nullable=False)
+    house_name = Column(String(50), nullable=True)
+    
+    
     def to_dict(self) -> dict:
         return {
-            "id": self.id,
-            "user_id": self.user_id,
-            "house_name": self.house_name,
-            "postal_code": self.postal_code,
+            "id": self.id, "user_id": self.user_id,
+            "house_street": self.house_street,
             "house_number": self.house_number,
             "house_complement": self.house_complement,
+            "city": self.city,
+            "house_neighborhood": self.house_neighborhood,
+            "postal_code": self.postal_code,
+            "house_name": self.house_name
             }
