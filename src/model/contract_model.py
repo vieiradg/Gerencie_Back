26c51633ded0b7de4e8a40af6bd71b0291ea3c33
PatrickLAF2k <patrickleonardoaf@gmail.com>
2025-09-21 20:19:56 -0300

    corrigido a forma de retornar a data

--- src/model/contract_model.py
+++ src/model/contract_model.py
@@ -1,8 +1,9 @@
 from src.model import db
 from sqlalchemy import Column, Integer, Float, String, ForeignKey, Date
 
+
 class contractModel(db.Model):
-    __tablename__ = 'contracts'
+    __tablename__ = "contracts"
 
     # Campos obrigatórios de cadastro
     id = Column(Integer, primary_key=True, autoincrement=True)
@@ -10,11 +11,10 @@ class contractModel(db.Model):
     tenant_id = Column(Integer, ForeignKey("tenants.id"), nullable=False)
     property_id = Column(Integer, ForeignKey("properties.id"), nullable=False)
     lease_period = Column(Integer, nullable=False)  # agora inteiro (meses)
-    rent_value = Column(Float, nullable=False)      # valor do aluguel como float
-    due_day = Column(Integer, nullable=False)       # dia do vencimento como inteiro
+    rent_value = Column(Float, nullable=False)  # valor do aluguel como float
+    due_day = Column(Integer, nullable=False)  # dia do vencimento como inteiro
     start_date = Column(Date, nullable=False)
     status = Column(Integer, nullable=False, default=0)
-
 
     def to_dict(self) -> dict:
         return {
@@ -25,6 +25,8 @@ class contractModel(db.Model):
             "lease_period": self.lease_period,
             "rent_value": self.rent_value,
             "due_day": self.due_day,
-            "start_date": self.start_date.isoformat() if self.start_date else None,
-            "status": self.status
+            "start_date": (
+                self.start_date.strftime("%d/%m/%Y") if self.start_date else None
+            ),
+            "status": self.status,
         }
