ab5a37bec6b70e297ad1a63304a6c528aefca197
PatrickLAF2k <patrickleonardoaf@gmail.com>
2025-09-28 17:28:14 -0300

    Refatorado a lógica de registro de locatários para melhorar a legibilidade e o tratamento de erros

--- src/controller/tenants/tenant_register.py
+++ src/controller/tenants/tenant_register.py
@@ -6,6 +6,7 @@ from src.model import db
 from src.security.jwt_config import token_required
 from . import bp_tenant
 
+
 @bp_tenant.route("/register", methods=["POST"])
 @token_required
 @swag_from("../../docs/tenant_register.yml")
@@ -13,32 +14,34 @@ def register(user_data):
      data = request.get_json()
      user_id = user_data["id"]
 
-     tenant_exists = db.session.execute(
-          db.select(tenantModel).where(
-               or_(
-                    tenantModel.phone_number == data["phone_number"],
-                    tenantModel.cpf == data["cpf"]
-               )
-          )
-     ).scalar()
-          
+     for field in ["name", "cpf", "phone_number"]:
+        if not data.get(field):
+            return jsonify({"message": "Todos os campos são obrigatórios."}), 400
+
+     tenant_exists = tenantModel.query.filter(
+         or_( tenantModel.phone_number == data["phone_number"], tenantModel.cpf == data["cpf"], )
+     ).first()
+
+
      if tenant_exists:
-      if tenant_exists.phone_number == data["phone_number"]:
-        return jsonify({"message": "Telefone já cadastrado"}), 400
-      if tenant_exists.cpf == data["cpf"]:
-        return jsonify({"message": "CPF já cadastrado"}), 400
-          
+         if tenant_exists.phone_number == data["phone_number"]:
+             return jsonify({"message": "Numero de telefone já cadastrado."}), 400
+         if tenant_exists.cpf == data["cpf"]:
+             return jsonify({"message": "Inquilino já cadastrado."}), 400    
      tenant = tenantModel(
-          name=data["name"],
-          user_id = user_id,
-          phone_number=data["phone_number"],
-          cpf=data["cpf"]
-     )
+         name=data["name"],
+         user_id=user_id,
+         phone_number=data["phone_number"],
+         cpf=data["cpf"],
+     )    
 
      try:
-          db.session.add(tenant)
-          db.session.commit()
-          return jsonify({ "message": "Inquilino cadastrado com sucesso", "tenant": tenant.to_dict() }), 201
+         db.session.add(tenant)
+         db.session.commit()
+         return (
+             jsonify( { "message": "Inquilino cadastrado com sucesso.", "tenant": tenant.to_dict(), } ),
+             201,
+         )
      except Exception as e:
-          db.session.rollback()
-          return jsonify({"message": "Erro ao cadastrar Inquilino", "error": str(e)}), 500
+         db.session.rollback()
+         return jsonify({"message": "Erro ao cadastrar Inquilino", "error": str(e)}), 500
